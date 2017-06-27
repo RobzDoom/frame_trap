557c152bd7b92e16596b64d195e6ec0fdbcab6e2
robzdoom <jorgecruz.rd@gmail.com>
2017-06-27 16:20:31 -0700

    added new fonts for website

--- packages/react-scripts/template/src/components/body.js
+++ packages/react-scripts/template/src/components/body.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Body extends Component {
   render() {
     return (
-      <div>
+      <div className="main_body">
         <h1>Thsis is the Body ok</h1>
       </div>
     );
