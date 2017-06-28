114446aa3001690461eb85214bc68091a741fb9d
robzdoom <jorgecruz.rd@gmail.com>
2017-06-28 12:53:03 -0700

    added folder with images

--- packages/react-scripts/template/src/components/body.js
+++ packages/react-scripts/template/src/components/body.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import sflogo from '../images/sfv_logo.png';
 
 class Body extends Component {
   render() {
     return (
       <div className="main_body">
         <h1>Choose a Game</h1>
+        <div><p>This is where the games will go</p></div>
+        <img src={sflogo} alt="Stret Figher 5" />
 
       </div>
     );
